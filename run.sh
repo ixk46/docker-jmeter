4ba71de63751e2d8ec932edec795983a3ca9016c
ixk46 <ikwhan.kim@equifax.com>
2021-02-26 05:43:28 +0000

    run the image from the internal ecr

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 # Run JMeter Docker image with options
 
 NAME="jmeter"
-IMAGE="justb4/jmeter:5.3"
+# IMAGE="justb4/jmeter:5.3"
+IMAGE="725485651984.dkr.ecr.ca-central-1.amazonaws.com/efx-intl-ca-dev-jmeter:5.3"
 
 # Finally run
 sudo docker stop ${NAME} > /dev/null 2>&1
